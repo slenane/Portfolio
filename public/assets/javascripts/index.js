1a57bc394084fd53581c5915a8e8abbf817bf830
slenane <slenane1@gmail.com>
2021-11-07 18:46:14 +0000

    Project section and image updates

---
+++ public/assets/javascripts/index.js
@@ -0,0 +1,53 @@
+const projectToggleShowBtns = document.querySelectorAll(".project_item--read_more");
+
+const toggleShowProject = (e) => {
+    let project = e.target.closest('.project_item');
+    let stack = project.querySelector('.project_stack');
+    let body = project.querySelector('.project_body');
+    let images = project.querySelector('.project_images');
+    let mainImage = project.querySelector('.project_image--main');
+    let secondaryImageDiv = project.querySelector('.project_secondary_images');
+    let secondaryImages = project.querySelectorAll('.project_image--secondary');
+
+    if (e.target.classList.contains('closed')) {
+        // Animate the removal of the stack
+        stack.classList.add('stack_shrink');
+        // Hide the main image
+        mainImage.classList.add('slide_out');
+        setTimeout(() => {
+            // Order the images correctly
+            images.classList.add('open');
+            // Show secondary images
+            secondaryImageDiv.classList.remove('hide');
+            secondaryImages.forEach(img => img.classList.add('slide_in'));
+            // Show the project body
+            body.classList.add('project_body--open');
+            // Set the project to be shown
+            e.target.classList.remove("closed");
+            e.target.textContent = "Read less";
+        }, 400);
+    } else {
+        // Animate the showing of the stack
+        stack.classList.remove('stack_shrink');
+        // Hide secondary images
+        secondaryImageDiv.classList.add('hide');
+        secondaryImages.forEach(img => img.classList.remove('slide_in'));
+        // Order the images correctly
+        images.classList.remove('open');
+        // Show the main image
+        mainImage.classList.remove('slide_out');
+        // Hide the project body
+        body.classList.remove('project_body--open');
+        // Set the project to be closed
+        e.target.classList.add("closed");
+        e.target.textContent = "Read more";
+
+        // Set the scroll position to the top of the project div
+        window.scrollTo({
+            top: project.offsetTop - 100,
+            behaviour: "smooth"
+        });
+    }
+};
+
+projectToggleShowBtns.forEach(btn => btn.addEventListener('click', toggleShowProject));
