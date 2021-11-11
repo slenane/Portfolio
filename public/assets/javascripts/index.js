56244128ee49692bf65237262a70e5e847cfa5ed
slenane <slenane1@gmail.com>
2021-11-11 22:58:23 +0000

    Added new Images - Updated project section design

--- public/assets/javascripts/index.js
+++ public/assets/javascripts/index.js
@@ -1,32 +1,33 @@
 const projectToggleShowBtns = document.querySelectorAll(".project_item--read_more");
+const bounce = document.querySelectorAll('.bouncing_icon');
 
 const toggleShowProject = (e) => {
     let project = e.target.closest('.project_item');
     let stack = project.querySelector('.project_stack');
     let stackLarge = project.querySelector('.project_open--stack');
     let body = project.querySelector('.project_body');
-    let secondaryImages = project.querySelector('.secondary_images');
+    let images = project.querySelector('.project_images');
+    let secondaryImages = project.querySelectorAll('.secondary_images');
     let blurb = project.querySelector('.project_blurb');
-    
     let projectLinks = project.querySelector('.project_links');
     let chessChallenge =  project.querySelector('.chess--challenge_link');
   
-
     if (e.target.classList.contains('closed')) {
-        // Hide the blurb (set )
+        // Hide the blurb (set max-height and opacity to 0)
         blurb.classList.add('blurb_shrink');
-
         // BEGIN TIMEOUT
         setTimeout(() => {
             // Set the project to be open (increase margin-bottom)
             project.classList.add('project_open');
+            // Add gap between the main and secondary images
+            images.classList.add('images_open');
+            // Show the additional images
+            secondaryImages.forEach(img => img.classList.add('show_secondary_images'));
             // Show the project description
             body.classList.add('project_body--open');
             // Update the read more button
             e.target.classList.remove("closed");
             e.target.textContent = "Read less";
-            // Show the additional images
-            secondaryImages.classList.add('show_secondary_images');
             // Remove the small stack
             stack.classList.add('hide_stack');
             // Show the large stack
@@ -37,10 +38,11 @@ const toggleShowProject = (e) => {
                 chessChallenge.classList.remove("hide");
             }
         }, 400);
-
     } else {
+        // Remove gap between the main and secondary images
+        images.classList.remove('images_open');
         // Hide secondary images
-        secondaryImages.classList.remove('show_secondary_images');
+        secondaryImages.forEach(img => img.classList.remove('show_secondary_images'));
         // Show the project blurb
         blurb.classList.remove('blurb_shrink');
         // Hide the large stack
@@ -57,16 +59,20 @@ const toggleShowProject = (e) => {
             projectLinks.classList.remove("chess_links");
             chessChallenge.classList.add("hide");
         }
-
         // Set the scroll position to the top of the project div
         window.scrollTo({
             top: project.offsetTop - 100,
             behaviour: "smooth"
         });
-
         // Set the project to be closed (reduce margin-bottom)
         project.classList.remove('project_open');
     }
 };
 
+const stopBounceAnimation = (e) => {
+    let stack = e.target.closest('.project_open--stack');
+    stack.classList.remove('bouncing_icon');
+};
+
 projectToggleShowBtns.forEach(btn => btn.addEventListener('click', toggleShowProject));
+bounce.forEach(stack => stack.addEventListener('mouseenter', stopBounceAnimation));
